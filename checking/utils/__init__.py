0ecef71a6f1d4a517680328e3332c33b2e3fc5d1
Jhon Honce <jhonce@redhat.com>
2018-06-07 09:01:54 -0700

    Add sections 1.6 and 1.7
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- checking/utils/__init__.py
+++ checking/utils/__init__.py
@@ -1,5 +1,6 @@
 """Helper Classes for python checks."""
 
+import os
 import re
 import subprocess
 import sys
@@ -28,13 +29,12 @@ def resolve_config(path):
     try:
         cmd = 'set -o allexport; . {} >&/dev/null; env'.format(path)
         out = subprocess.check_output(cmd, close_fds=True, shell=True, env={})
-    except (subprocess.CalledProcessError):
+    except subprocess.CalledProcessError:
         env = {}
     else:
         env = {
             k: v
-            for k, v in
-            (x.split('=', 1) for x in out.splitlines() if '=' in x)
+            for k, v in (x.split('=', 1) for x in out.splitlines() if '=' in x)
         }
         for k in ('SHLVL', 'PWD', '_'):
             env.pop(k, None)
@@ -44,18 +44,32 @@ def resolve_config(path):
 
 def strip_config(path):
     """Return lines stripped of comments from configuration file."""
-    lines, data = [], []
     try:
         with open(path, 'r') as f:
-            data = f.readlines()
+            for line in f.readline():
+                cooked = re.sub(r'#.*$', '', line.strip())
+                if len(cooked) > 0:
+                    yield cooked
     except IOError as e:
         sys.stderr.write(e.message + '\n')
 
-    for line in data:
-        cooked = re.sub('^ *#.*', '', line.strip())
-        if len(cooked) > 0:
-            lines.append(cooked)
-    return lines
+
+def grep(pattern, path):
+    """Search file for pattern."""
+    _re = pattern if hasattr(pattern, 'search') else re.compile(pattern)
+    for line in strip_config(path):
+        if _re.search(line):
+            yield line
+
+
+def find_files(*args):
+    for entry in args:
+        if os.path.isfile(entry):
+            yield entry
+        else:
+            for d, _, files in os.walk(entry):
+                for f in files:
+                    yield os.path.join(d, f)
 
 
 def log_error(msg):  # pragma: no cover
